90848763ba978470535cb25f7e427f8a99b384fc
Vinh Hoang <hoangqv92@gmail.com>
2021-01-27 17:46:44 -0800

    propsTest

--- todo-refresher/src/App.js
+++ todo-refresher/src/App.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import Form from './components/Form';
+import Todolist from './components/TodoList';
 
 function App() {
+  const [inputText, setInputText] = useState("");
   return (
     <div className="App">
       <header>
         <h1>Vinh's Todo List</h1>
       </header>
-      <Form />
+      <Form setInputText={setInputText} />
+      <Todolist />
     </div>
   );
 }
